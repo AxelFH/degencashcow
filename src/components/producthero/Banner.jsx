5cf3a8a66419abe83b54e4f17d81e969c49124fe
AxelFH <axel@vulcanics.mx>
2024-08-01 14:44:00 -0600

    fixes

--- src/components/producthero/Banner.jsx
+++ src/components/producthero/Banner.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
-import bgImage from '../../assets/images/bgm.png'; // Ajusta la ruta a la imagen de fondo
-import charImage from '../../assets/images/cow/banner.png'; // Ajusta la ruta a la imagen del personaje
+import charImage from '../../assets/images/cow/cowi.gif'; // Ajusta la ruta a la imagen del personaje
+import cloudImage from '../../assets/images/cow/cloud.png'; // Ajusta la ruta a la imagen de la nube
 
 const ProfessorSection = () => {
     return (
         <div className="professor-section">
-            <div className="text-container">
-                <h1>Mooooo</h1>
-                <h3>Welcome to the Moost Degen Cash Cow on the Solana Blockchain. Join the DegenFi Moovement  💰🐄</h3>
+            <div className="cloud-container">
+                <img className="cloud cloud1" src={cloudImage} alt="Nube" />
+                <img className="cloud cloud2" src={cloudImage} alt="Nube" />
+                <img className="cloud cloud3" src={cloudImage} alt="Nube" />
+                <img className="cloud cloud4" src={cloudImage} alt="Nube" />
+                <img className="cloud cloud5" src={cloudImage} alt="Nube" />
+                <img className="cloud cloud6" src={cloudImage} alt="Nube" />
             </div>
-            <img
-                className="character-image"
-                src={charImage}
-                alt="Personaje"
-            />
-
+            
+            <div className="circle-container">
+                <img
+                    className="character-image"
+                    src={charImage}
+                    alt="Personaje"
+                />
+            </div>
+            <div className="text-container">
+                <h1>Welcooooooooooome</h1>
+                <h3>Welcome to the Moost Degen Cash Cow on the Solana Blockchain. Join the DegenFi Moovement 💰🐄</h3>
+            </div>
         </div>
     );
 };
